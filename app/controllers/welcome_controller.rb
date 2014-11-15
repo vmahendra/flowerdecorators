50fbb6161b8413240656aba9c33d32f6964587fe
vmahendra <vijaymahendra85@gmail.com>
2014-11-15 12:47:47 +0530

    aa

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,28 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def aboutus
+  end
+
+  def contactus
+  end
+
+  def specifications
+  end
+  
+  def gallery
+  end
+
+  def feedback
+  end
+
+  def services
+  end
+  def reception
+    
+  end
+
+  def new
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flower::Application.config.secret_token = '5641fb53ccbfe4e07f46debfd9a698a1db6d8ec337906ec82b92003eadbb57d4bd71f59fc0f1437a89f25738d35f784ff5f791d0725d9a2b5e50fef5ee37a6ea'
